de428bf1ba05644605c51690b64ff79ae95244c0
Lucas Chen <chucobo5219@gmail.com>
2017-10-23 03:58:22 +0800

    Correct the image url

--- lib/image_kits/downloader.rb
+++ lib/image_kits/downloader.rb
@@ -2,10 +2,9 @@ require 'image_kits/image_kit.rb'
 require 'open-uri'
 class ImageKit::Downloader < ImageKit
   PATH = ENV['IMAGE_PATH']
-  def download(_url, filename)
+  def download(url, filename)
     open(PATH + filename, 'wb') do |file|
-      test_path= 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
-      file << open(test_path).read
+      file << open(url).read
     end
   end
 end

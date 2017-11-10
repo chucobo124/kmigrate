f0f69bb237724fef89732e409aba545de157da15
Lucas Chen <chucobo5219@gmail.com>
2017-11-10 22:03:35 +0800

    mapping category

--- app/controllers/category_maps_controller.rb
+++ app/controllers/category_maps_controller.rb
@@ -64,25 +64,30 @@ class CategoryMapsController < ApplicationController
 
   def upload_with_csv
     csv = CSV.parse(csv_file.open.read)
-    categories = {}
     api_collections = get_collections
     CategoryMap.all.delete_all
     csv.drop(2).each do |row|
+      category = {}
       carousell_category = row[4]
       carousell_sub_category = row[5]
 
-      categories[:kktwon_category] = row[0]
-      categories[:kktwon_sub_category] = row[1]
-      categories[:carousell_category] = carousell_category
-      categories[:carousell_sub_category] = carousell_sub_category
-
+      category[:kktwon_category] = row[0]
+      category[:kktwon_sub_category] = row[1]
+      category[:carousell_category] = carousell_category
+      category[:carousell_sub_category] = carousell_sub_category
+      carousell_category = carousell_category.delete("&").delete(",").delete(" ")
+      carousell_sub_category = carousell_sub_category.delete("&").delete(",").delete(" ")
       if api_collections[carousell_category].present?
-        categories[:carousell_category_id] = api_collections[carousell_category]
-      elsif api_collections[carousell_sub_category].present?
-        categories[:carousell_category_id] = api_collections[carousell_sub_category]
+        if api_collections[carousell_category][carousell_sub_category].present?
+          category[:carousell_category_id] = api_collections[carousell_category][carousell_sub_category][:id]
+        else
+          category[:carousell_category_id] = "/NA"
+        end
+      else
+        category[:carousell_category_id] = "/NA"
       end
 
-      CategoryMap.create(categories)
+      CategoryMap.create(category)
     end.compact
     redirect_to category_maps_path
   end
@@ -107,6 +112,7 @@ class CategoryMapsController < ApplicationController
     retry_count = 0
     uri = URI.parse(CARO_COLLECTION)
     resp = Net::HTTP.get_response(uri)
+
     while (resp.code == '301' || resp.code == '302') && (retry_count < CONNECTION_RETRY)
       if resp.header['location'].present?
         resp = Net::HTTP.get_response(URI.parse(resp.header['location']))
@@ -118,11 +124,30 @@ class CategoryMapsController < ApplicationController
     return unless resp.body.present?
     collection_lists = JSON.parse(resp.body, symbolize_names: true)
 
-    collection = {}
+    collection = pointer_collection = {}
     if collection_lists.present?
       collection_lists.each do |collection_list|
-        name = collection_list[:name]
-        collection[name] = collection_list[:id]
+        name = collection_list[:name].delete("&").delete(",").delete(" ")
+        pointer_collection[name] = {}
+        pointer_collection[name][:id] = collection_list[:id]
+
+        if collection_list[:subcategories].present?
+          pointer_collection[name] = pointer_collection[name].merge(loop_subcategory(collection_list[:subcategories]))
+        end
+      end
+    end
+    collection
+  end
+
+  def loop_subcategory(collection_list)
+    collection = pointer_collection = {}
+    collection_list.each do |sub_collection|
+      name = sub_collection[:name].delete("&").delete(",").delete(" ")
+      pointer_collection[name] = {}
+      pointer_collection[name][:id] = sub_collection[:id]
+
+      if sub_collection[:subcategories].present?
+        pointer_collection[name] = pointer_collection[name].merge(loop_subcategory(sub_collection[:subcategories]))
       end
     end
     collection
